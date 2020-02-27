93cbb6567627c29e7590e8399ea8929f29b06785
Galder Zamarreño <galder@zamarreno.com>
2020-02-27 11:56:38 +0100

    Shorter title, fix make and add correct test

--- Formula/openlibm.rb
+++ Formula/openlibm.rb
@@ -1,5 +1,5 @@
 class Openlibm < Formula
-  desc "High quality system independent, portable, open source libm implementation"
+  desc "High quality, portable, open source libm implementation"
   homepage "https://openlibm.org"
   url "https://github.com/JuliaMath/openlibm/archive/v0.7.0.tar.gz"
   sha256 "1699f773198018b55b12631db9c1801fe3ed191e618a1ee1be743f4570ae06a3"
@@ -9,7 +9,7 @@ class Openlibm < Formula
     (lib/"pkgconfig").mkpath
     (include/"openlibm").mkpath
 
-    system "make", "install", "prefix=."
+    system "make", "install", "prefix=#{prefix}"
 
     lib.install Dir["lib/*"].reject { |f| File.directory? f }
     (lib/"pkgconfig").install Dir["lib/pkgconfig/*"]
@@ -17,6 +17,15 @@ class Openlibm < Formula
   end
 
   test do
-    system "make", "test"
+    (testpath/"test.c").write <<~EOS
+    #include <stdio.h>
+    #include "openlibm.h"
+    int main (void) {
+      printf("%.1f\n", cos(acos(0.0)));
+    }
+  EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-llz",
+           "-o", "test"
+    assert_equal "0.0", shell_output("./test")
   end
 end
